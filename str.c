5cf14e14381fadec9633d4343d0677529cb8485e
horazio <hfusco@stuy.edu>
2018-09-28 15:36:30 -0400

    added a working length function, working on strncpy. strncpy works if the n is not out of bounds

--- str.c
+++ str.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <string.h>
+#include "str.h"
 
-//char * strncpy(char * dest, char * orig, int n){
-  
-  
-//}
+int mystrlen( char * str ){
+  return * str? 1 + mystrlen(++str):0;
+}
+
+char * mystrncpy(char * dest, char * orig, int n){
+  char * p = orig;
+  char * p2 = dest;
+  while(p - orig - n){
+    *(p2++) = *(p++);
+  }
+  return dest;
+}
+
 
 
 int main(){
-  char s1[10] = "hello";
-  char * s2 = "world";
+  char s1[10] = "";
+  char * s2 = "wo";
+  int i = 30;
   
-  // printf("strncpy says: %s", strncpy(s1, s2, 3);
+  printf("strlen says: %ld, mystrlen says: %d \n", strlen(s1), mystrlen(s1));
+  //printf("strncpy says: %s, mystrncpy says: %s \n", strncpy(s1, s2, i), mystrncpy(s1, s2, i));
   
   return 0;
 }
