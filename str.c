7c579ac41a5607ff7a7fa02aa511b49a0d4c46a8
horazio <hfusco@stuy.edu>
2018-09-28 15:12:34 -0400

    makefile works and compiles the str files

--- str.c
+++ str.c
@@ -8,10 +8,10 @@
 
 
 int main(){
-  char s1[10] = "hello"
+  char s1[10] = "hello";
   char * s2 = "world";
-
-  printf("strncpy says: %s", strncpy(s1, s2, 3);
+  
+  // printf("strncpy says: %s", strncpy(s1, s2, 3);
   
   return 0;
 }
---
+++ str.h
@@ -0,0 +1,11 @@
+int mystrlen( char * );
+
+char * mystrncpy( char *dest, char *source, int n);
+
+char * mystrncat( char *dest, char *source, int n);
+
+int mystrcmp( char *s1, char *s2 );
+
+char * mystrchr( char *s, char c );
+
+char * mystrstr( char *s1, char * s2 );
