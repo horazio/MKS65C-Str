acff97de0acc437d14d8e7e0c8b222a65e2036ec
horazio <hfusco@stuy.edu>
2018-09-29 21:25:51 -0400

    added the strchr function

--- str.c
+++ str.c
@@ -26,8 +26,8 @@ char * mystrncat( char * dest, char * orig, int n){
 
 char * mystrchr( char *s, char c ){
   while(*(s++)){
-    if(*(s) = c){
-      reurn s;
+    if(*(s) == c){
+      return s;
     }
   }
   return 0; 
@@ -37,14 +37,15 @@ int main(){
   char s1[10] = "qwe";
   char * s2 = "qwertyuiop";
   int i = 4;
-  char c = 'p'
+  char c = '3';
 
   //printf("%s \n", strncpy(s1,s2,i));
   
   //printf("strlen says: %ld, mystrlen says: %d \n", strlen(s1), mystrlen(s1));
   //printf("strncpy says: %s, mystrncpy says: %s \n", strncpy(s1, s2, i), mystrncpy(s1, s2, i));
   //printf("strncat says: %s, mystrncat says: %s \n", strncat(s1, s2, i), mystrncat(s1, s2, i));
-  printf("strchr says: %s, mystrchr says: %s \n", strncat(s2, i), mystrncat(s2, i));
+
+  printf("strchr says: %p, mystrchr says: %p  \n", strchr(s2, c), mystrchr(s2, c));
 
   return 0;
 }
